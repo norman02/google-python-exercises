c8c783b785c22e7affb4bcaa41447491a8b8d9db
norman <normanj777@gmail.com>
2024-10-21 15:32:16 -0400

    implement both_ends

--- basic/string1.py
+++ basic/string1.py
@@ -38,6 +38,10 @@ def donuts(count):
 # is less than 2, return instead the empty string.
 def both_ends(s):
   # +++your code here+++
+  if len(s) < 2 :
+      return ''
+  else : 
+      return s[:2] + s[-2:]
   return
 
 
